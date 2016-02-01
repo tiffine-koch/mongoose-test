19d1986abbaf1c3a16e62d2ed8ba6ea3b3afb961
Apple <tiffine.koch@gmail.com>
2016-02-01 15:55:30 -0800

    working file, from cade's lecture

--- models/person.js
+++ models/person.js
@@ -5,41 +5,23 @@ var moment = require('moment');
 var Person;
 
 var personSchema = mongoose.Schema({
-  occupation: String,
+  company: String,
   name: {first: String,
     last: String},
-  age: {type: Number, min: 1, max: 100},
-  likes: [String],
-  dislikes: [String],
   gender: String,
-  education: String,
+  likes: [{String}],
+  dislikes: [{String}],
+  age: {type: Number, min: 1, max: 100},
+  // education: String,
   birthday: {type: Date},
   createdAt: {type: Date, default: Date.now }
-})
+});
 
 //age linked with birthday
 personSchema.pre('save', function(next) {
   this.age = moment().diff(moment(this.birthday), 'years');
   next();
 });
-
-personSchema.statics.create = function(personObj, callback){
-  var person = new Person(personObj);
-  person.save(callback)
-}
-
-personSchema.statics.showAll = function(req, callback){
-  Person.find({}, function(err, people){
-    callback(err, people);
-  });
-}
-
-personSchema.statics.doQuery = function(req, callback){
-  console.log("req", req.body.query);
-  Person.find(req.body.query, function(err, people){
-    callback(err, people);
-  });
-}
 
 Person = mongoose.model('Person', personSchema);
 // Person = mongoose.model('person', personSchema);
--- routes/persons.js
+++ routes/persons.js
@@ -5,22 +5,22 @@ var Person = require('../models/person');
 
 
 /* GET persons listing. */
-// router.get('/', function(req, res) {
-//   Person.find({}, function(err, persons) {
-//     res.status(err ? 400 : 200).send(err || persons);
-//   });
-// });
 router.get('/', function(req, res) {
-  req.query === {}; //no query
-  //GET localhost:3000/persons?company=MARAVANE&sort=age
-  //key/value pair separated by equal sign
-  //when we make the get request- special find by users
-  req.query === {company: "MARVANE"};
-
-  Person.find({}).limit(20).exec(function(err, persons) {
+  Person.find({}, function(err, persons) {
     res.status(err ? 400 : 200).send(err || persons);
   });
 });
+// router.get('/', function(req, res) {
+  // req.query === {}; //no query
+  //GET localhost:3000/persons?company=MARAVANE&sort=age
+  //key/value pair separated by equal sign
+  //when we make the get request- special find by users
+  // req.query === {company: "MARVANE"};
+
+//   Person.find({}).limit(20).exec(function(err, persons) {
+//     res.status(err ? 400 : 200).send(err || persons);
+//   });
+// });
 
 //same route different query strings - as many or as few
 router.post('/', function(req, res) {
